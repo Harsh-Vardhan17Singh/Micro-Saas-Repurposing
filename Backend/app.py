f604def17ffbd5cea1479ab0632844e9961bc273
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-03-29 23:27:14 +0530

    Adding Generate Route With Dummy Data And Testing the postman Part

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello! Backend Is Running"
+
+@app.route("/generate",methods =['POST'])
+def generate():
+    # 1.Get Text
+    data = request.get_json()
+
+    # 2. Validate
+    if not data or "text" not in data:
+        return jsonify({
+            "error":"Invalid Input"
+        })
+    
+    text = data.get("text")
+    
+    
+    #2.Print Data
+    print("Received text",text)
+
+    #3.get Dummy data
+    response = (f"Your text:{text}")
+
+    # 4.return jsonify
+    
+    return jsonify({
+        "twitter":response,
+        "linkedin":response,
+        "summary":response
+    
+    }) 
+    
+    
+
+
 
 
 if __name__ == "__main__":

412ea8e8c62150c6a4a2187fe8ecd86450e03ddc
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-04-30 00:37:28 +0530

    Fixing Database Issue of Same User

--- Backend/database.py
+++ Backend/database.py
@@ -1,7 +1,9 @@
 import sqlite3
-
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR,"user.db")
 def init_db():
-    conn = sqlite3.connect("user.db")
+    conn = sqlite3.connect(DB_PATH,check_same_thread=False,timeout=10)
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -10,32 +12,46 @@ def init_db():
             usage INTEGER DEFAULT 0,
             plan TEXT DEFAULT 'free'
                    )""")
+    
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_user_id ON users(user_id)")
+
     conn.commit()
     conn.close()
 
 def get_user(user_id):
-    conn = sqlite3.connect("user.db")
-    cursor = conn.cursor()
+    try:
+        conn = sqlite3.connect(DB_PATH,check_same_thread=False,timeout=10)
+        cursor = conn.cursor()
 
-    cursor.execute("SELECT usage, plan FROM users WHERE user_id = ?",(user_id,))
-    user = cursor.fetchone()
+        cursor.execute("SELECT usage, plan FROM users WHERE user_id = ?",(user_id,))
+        user = cursor.fetchone()
 
-    conn.close()
-    return user
+        conn.close()
+        return user 
+    except Exception as e:
+        print("DB ERROR",e)
+        return e
+
 
 def create_user(user_id):
-    conn = sqlite3.connect("user.db")
+    conn = sqlite3.connect(DB_PATH,check_same_thread=False,timeout=10)
     cursor = conn.cursor()
 
-    cursor.execute("INSERT INTO users (user_id) VALUES (?)",(user_id,))
+    cursor.execute("""INSERT OR IGNORE INTO users (user_id) VALUES (?)""",(user_id,))
     conn.commit()
     conn.close()
 
 def update_usage(user_id,usage):
-    conn = sqlite3.connect("user.db")
+    conn = sqlite3.connect(DB_PATH,check_same_thread=False,timeout=10)
     cursor = conn.cursor()
 
-    cursor.execute("UPDATE users SET usage = ? WHERE user_id = ?",(usage, user_id))
+    cursor.execute("""
+            UPDATE users SET usage = ?
+                   WHERE user_id = ?""", (usage,user_id))
+    if cursor.rowcount == 0 :
+        cursor.execute("""
+             INSERT INTO users (user_id,usage)
+                       VALUES(?, ?)""",(user_id,usage))
     conn.commit()
     conn.close()
     

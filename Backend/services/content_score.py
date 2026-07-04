ab53b070add08425d8228ad57cfbd007094dcae9
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-07-04 22:47:24 +0530

    Adding Content_score function

--- Backend/services/content_score.py
+++ Backend/services/content_score.py
@@ -11,3 +11,27 @@ CTA_WORDS = [
     "learn more",
     "join"
 ]
+
+def calculate_content_score(result):
+
+    score = {
+        "overall": 0,
+        "hook": 0,
+        "readability": 0,
+        "engagement": 0,
+        "cta": 0,
+        "feedback": []
+    }
+
+    text = ""
+
+    if "twitter" in result:
+        text += " ".join(result["twitter"]) + " "
+
+    if "linkedin" in result:
+        text += result["linkedin"] + " "
+
+    if "summary" in result:
+        text += result["summary"]
+
+    text = text.strip()
